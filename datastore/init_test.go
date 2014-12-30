55f7dc603ad633328c58d9f111f0b8ea8e7fcc55
maksadbek <a.maksadbek@gmail.com>
2014-12-30 20:25:31 +0500

    sql change

--- datastore/init_test.go
+++ datastore/init_test.go
@@ -1,9 +1,8 @@
-package datastore_test
+package datastore
 
 import (
 	"fmt"
 	"github.com/mmirolim/hack-project/conf"
-	ds "github.com/mmirolim/hack-project/datastore"
 	"log"
 	"strings"
 	"testing"
@@ -26,28 +25,25 @@ func TestInitialize(t *testing.T) {
 
 	fmt.Printf("%+v", App.DS)
 
-	db, err := ds.Initialize(App.DS)
+	db, err := Initialize(App.DS)
 	if err != nil {
 		log.Println(err)
 		t.Error(err)
 	}
 
-	order := ds.Order{
+	order := Order{
 		TableID: 1,
-		StaffID: 2,
+		StaffID: 12,
 	}
-	/*
-		_, err = db.Query("CREATE TABLE orders ( tableID INTEGER , staffID INTEGER, ID INTEGER, PRIMARY KEY(ID ASC))")
-		if err != nil {
-			t.Error(err)
-		}
-	*/
-	//_, err = db.Exec("INSERT INTO VALUES (1, ?,?)", order.TableID, order.StaffID)
-	_, err = db.Query("INSERT INTO orders VALUES (1, ?,?)", order.TableID, order.StaffID)
 
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS orders ( tableID INTEGER , staffID INTEGER, ID INTEGER, PRIMARY KEY(ID ASC))")
 	if err != nil {
 		t.Error(err)
 	}
-	db.Exec("INSERT INTO orders VALUES (1, 2,3)")
+
+	_, err = db.Exec("INSERT INTO orders VALUES (1, ?,?)", order.TableID, order.StaffID)
+	if err != nil {
+		t.Error(err)
+	}
 	fmt.Println("initialize pass")
 }
